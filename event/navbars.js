436569ee0d35dd4b044a4bb48dd0ee3ac1ebddc2
Vishal kumar <vishalk80269@gmail.com>
2023-01-05 03:24:49 +0530

    header complete

---
+++ event/navbars.js
@@ -0,0 +1,14 @@
+var barsValue= false;
+function tryDo(){
+
+if(!barsValue){
+   
+barsValue=true;
+document.getElementById("open").style.left='0%';
+ }else{
+     valsZ=false;
+ document.getElementById("open").style.left='100%';
+
+
+}
+}
